a1b35c9225889228169197b2c5f7c5ec89d10ca8
foorkan <furqanshabbir2010@gmail.com>
2020-03-31 22:32:33 -0500

    notes

--- loops.js
+++ loops.js
@@ -1,5 +1,14 @@
+//for loop syntax
+//first section is initialization/creation of the variable
+//second section is the condition that the loop will run in
+//third section is how the  variable will change after each loop
+
+
+
 //make a loop that will console log numbers 1-20 and squaring those numbers  //remember to run javascript files you put node and then name of the file
 for(let i=0; i<=20; i++){
     console.log(i*i);
 }
+
+
 
