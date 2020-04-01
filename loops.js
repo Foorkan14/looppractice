bc464780a834d1521811edfbd4e5f0bcc8c93ba0
foorkan <furqanshabbir2010@gmail.com>
2020-03-31 22:25:16 -0500

    first commit

---
+++ loops.js
@@ -0,0 +1,5 @@
+//make a loop that will console log numbers 1-20 and squaring those numbers  //remember to run javascript files you put node and then name of the file
+for(let i=0; i<=20; i++){
+    console.log(i*i);
+}
+
